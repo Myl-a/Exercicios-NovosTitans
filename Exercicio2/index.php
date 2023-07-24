15620fa491a46266c1e5d44d40228fd993c74fd3
Sâmyla <samylamaria14@gmail.com>
2023-07-24 13:34:33 -0300

    Adicionando o backend do exercicio 2

--- Exercicio2/index.php
+++ Exercicio2/index.php
@@ -16,5 +16,34 @@
                 <button type="button" class="btn-primary">Salvar</button>
             </div>
         </form>
+        <?php
+        // Verifica se o formulário e o numero foram preenchidos
+        if (isset($_POST['numero'])) {
+            $numeroEntrada = $_POST['numero'];
+            echo verificarDivisibilidade($numeroEntrada);
+        }
+            // Função que serve para verificar se um número é divisível por outro
+            function ehDivisivel($numero, $divisor) {
+                return $numero % $divisor === 0;
+            }
+            // Função que serve para verificar divisibilidade e retornar a mensagem
+            function verificarDivisibilidade($numero) {
+                $divisivelPor10 = ehDivisivel($numero, 10);
+                $divisivelPor5 = ehDivisivel($numero, 5);
+                $divisivelPor2 = ehDivisivel($numero, 2);
+            
+                if ($divisivelPor10 && $divisivelPor5 && $divisivelPor2) {
+                    return "$numero é divisível por 10, 5 e 2.";
+                } elseif ($divisivelPor10) {
+                    return "$numero é divisível por somente por 10.";
+                } elseif ($divisivelPor5) {
+                    return "$numero é divisível por somente por 5.";
+                } elseif ($divisivelPor2) {
+                    return "$numero é divisível por somente por 2.";
+                } else {
+                    return "$numero não é divisível por 10, 5 ou 2.";
+                }
+            }
+        ?>
     </body>
 </html>
