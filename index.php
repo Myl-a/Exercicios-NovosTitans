547937dcc47a5167dc994ca6f0d91d78e78f0b26
Sâmyla <samylamaria14@gmail.com>
2023-07-24 13:41:36 -0300

    Adicionando o frontend do indice

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title>Exercicios</title>
+</head>
+<body>
+    <h1>Indice</h1>
+    <ul>
+        <li>
+            <a href="/exercicio1">Exercicio 1</a>
+        </li>
+    </ul>
+</body>
+</html>
