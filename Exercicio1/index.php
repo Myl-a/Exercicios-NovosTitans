232517526029b8c76ffcf471e9ca4b1e323e5743
Sâmyla <samylamaria14@gmail.com>
2023-07-24 12:57:08 -0300

    Enviando o frontend

---
+++ Exercicio1/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Exercicio 1</title>
+        <link rel='stylesheet' href='style.css'>
+    </head>
+    <body>
+        <h1>Exercicio 1</h1>
+        <form action="/exercicio1" method="post">
+            <div>
+                <label for="value1">Digite o primeiro valor: </label>
+                <input type="number" name="value1" id="value1">
+            </div>
+            <div>
+                <label for="value2">Digite o segundo valor: </label>
+                <input type="number" name="value2" id="value2">
+            </div>
+            <button type="submit">Salvar</button>
+        </form>
+    </body>
+</html>
