3604483ef1076665af9fa98e0dd4cb7356a805a6
Sâmyla <samylamaria14@gmail.com>
2023-08-03 15:10:44 -0300

    Tentando corrigir o erro do branch do exercicio

---
+++ Exercicio2/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Exercicio 2</title>
+        <link rel='stylesheet' href='style.css'>
+    </head>
+    <body>
+        <h1>Exercicio 2</h1>
+        <h2>Verificar Divisibilidade</h2>
+        <form method="post" action="/exercicio2-php-logica/exercicio2">
+            <div>
+                <label for="number">Digite um número:</label>
+                <input type="number" id="numero" name="numero">
+            </div> 
+            <div>
+                <button type="button" class="btn-primary">Salvar</button>
+            </div>
+        </form>
+    </body>
+</html>
