2a46a86e06387b2ffbc18e6260f45901ea556cd0
Sâmyla <samylamaria14@gmail.com>
2023-08-07 14:21:31 -0300

    Tentando corrigir o erro do branch

--- Exercicio1/index.php
+++ Exercicio1/index.php
@@ -17,5 +17,18 @@
             </div>
             <button type="submit">Salvar</button>
         </form>
+        <?php
+            if (isset($_POST['value1']) && isset($_POST['value2'])){
+                $value1 = $_POST['value1'];
+                $value2 = $_POST['value2'];
+                $total = $value1 + $value2;
+                if ($total > 20){
+                    $total = $total + 8;
+                } else{
+                    $total = $total - 5;
+                }
+                echo "<div class='result'>O resultado foi: $total</div>";
+            }
+        ?>
     </body>
 </html>
