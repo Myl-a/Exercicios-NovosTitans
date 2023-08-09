7fc6c6f221656ca97b2610d08880a9a8348b4e3e
Sâmyla <samylamaria14@gmail.com>
2023-08-09 13:41:17 -0300

    Corrigindo o backend do exercicio 5

--- Exercicio5/index.php
+++ Exercicio5/index.php
@@ -2,9 +2,6 @@
 <head>
     <title>Classificação de Triângulo</title>
     <link rel='stylesheet' href='style.css'>
-    <style>
-        /* Estilos CSS aqui */
-    </style>
 </head>
 <body>
     <h1>Classificação de Triângulo</h1>
@@ -26,7 +23,6 @@
                 </fieldset>
                 <button type="submit" name="verificar" class="btn-primary">Verificar Classificação</button>
             </form>
-            
             <?php
             function classificarTriangulo($lado1, $lado2, $lado3) {
                 if ($lado1 === $lado2 && $lado2 === $lado3) {
@@ -37,18 +33,14 @@
                     return "Triângulo Escaleno";
                 }
             }
-
             if (isset($_POST['verificar'])) {
                 $lado1 = floatval($_POST['lado1']);
                 $lado2 = floatval($_POST['lado2']);
                 $lado3 = floatval($_POST['lado3']);
-                
                 $classificacao = classificarTriangulo($lado1, $lado2, $lado3);
-                
                 echo "<script>alert('Classificação: $classificacao');</script>";
             }
             ?>
-            
         </div>
     </fieldset>
 </body>
