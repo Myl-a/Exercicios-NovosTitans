d2fbf9f54641714957e23b1c7c6766ca06783cc7
Sâmyla Ferreira <samylamaria14@gmail.com>
2023-08-15 14:37:03 -0300

    Atualizando o index

--- index.php
+++ index.php
@@ -9,5 +9,15 @@
             <a href="/exercicio1">Exercicio 1</a>
         </li>
     </ul>
+    <ul>
+        <li>
+            <a href="/exercicio2">Exercicio 2</a>
+        </li>
+    </ul>
+    <ul>
+        <li>
+            <a href="/exercicio3">Exercicio 3</a>
+        </li>
+    </ul>
 </body>
 </html>
