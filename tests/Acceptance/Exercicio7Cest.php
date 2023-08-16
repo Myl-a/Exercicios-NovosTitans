4d9e4f4bc3856792d2dc8aa0bd493d5c5af4368a
Sâmyla Ferreira <samylamaria14@gmail.com>
2023-08-16 15:19:55 -0300

    Criando o primeiro teste e inicializando o codecption

---
+++ tests/Acceptance/Exercicio7Cest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace Tests\Acceptance;
+
+use Tests\Support\AcceptanceTester;
+
+class Exercicio7Cest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+    }
+}
