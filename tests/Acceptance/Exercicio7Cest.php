ee0629b9c9beedad9e47a2c6865a4ed5d84ab801
Sâmyla Ferreira <samylamaria14@gmail.com>
2023-08-16 15:31:07 -0300

    Tentando fazer os testes automatizados

--- tests/Acceptance/Exercicio7Cest.php
+++ tests/Acceptance/Exercicio7Cest.php
@@ -12,7 +12,26 @@ class Exercicio7Cest
     }
 
     // tests
-    public function tryToTest(AcceptanceTester $I)
+    public function TittleExercicio7Test(AcceptanceTester $I)
     {
+        /**
+         * A biblioteca de uma universidade deseja fazer um algoritmo que leia o nome do livro que
+         * será emprestado, o tipo de usuário (professor ou aluno) e possa imprimir um recibo
+         * conforme mostrado a seguir. Considerar que
+         * o professor tem 10 dias para devolver o livro
+         * o aluno somente 3 dias
+         */
+        //Eu estou na pagina "exercicio 7"
+        //Eu vejo o titulo "Exercicio 7"
+    }
+    public function FormExercicio7Test(AcceptanceTester $I)
+    {
+        /**
+         * A biblioteca de uma universidade deseja fazer um algoritmo que leia o nome do livro que
+         * será emprestado, o tipo de usuário (professor ou aluno) e possa imprimir um recibo
+         * conforme mostrado a seguir. Considerar que
+         * o professor tem 10 dias para devolver o livro
+         * o aluno somente 3 dias
+         */
     }
 }
