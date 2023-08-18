16a8fc989e8dabfd6eaac2009b8b5202c6e084d5
Sâmyla Ferreira <samylamaria14@gmail.com>
2023-08-18 16:36:30 -0300

    Adicionando o backend do exercicio 7 com teste automatizado

---
+++ tests/Acceptance/Exercicio7Cest.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace Tests\Acceptance;
+
+use Tests\Support\AcceptanceTester;
+
+class Exercicio7Cest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function TitleExercicio7Test(AcceptanceTester $I)
+    {
+        $I->amOnPage('/Exercicio7');
+        $I->seeInTitle('Biblioteca Universitária');
+        $I->see('Biblioteca Universitária', 'h1');
+    }
+
+    public function FormExercicio7Test(AcceptanceTester $I)
+    {
+        $I->amOnPage('/Exercicio7');
+        $I->fillField('book-name', 'livro');
+        $I->fillField('user-type', 'tipoUsuario');
+    }
+}
