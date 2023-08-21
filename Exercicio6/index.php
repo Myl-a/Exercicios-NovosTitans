71b79b9b4ee89c18d977f2c5f5e819dbfba706e0
Sâmyla Ferreira <samylamaria14@gmail.com>
2023-08-21 11:58:02 -0300

    Tentando corrigir erros

--- Exercicio6/index.php
+++ Exercicio6/index.php
@@ -7,8 +7,8 @@
     <div class="container">
         <h1>Consulta de Mês</h1>
         <form>
-            <label for="numero">Digite um número inteiro entre 1 e 12:</label>
-            <input type="number" id="numero" name="numero" min="1" max="12" required>
+            <label for="numeroMes">Digite um número de mês entre 1 e 12:</label>
+            <input type="number" id="numeroMes" name="numeroMes" min="1" max="12" required>
             <button type="submit"><strong>Consultar</strong></button>
         </form>
         <div class="resultado" id="resultado">
--- index.php
+++ index.php
@@ -1,23 +1,63 @@
 <html>
 <head>
-    <title>Exercicios</title>
+    <title>Página de Índice</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>Indice</h1>
-    <ul>
-        <li>
-            <a href="/exercicio1">Exercicio 1</a>
-        </li>
-    </ul>
-    <ul>
-        <li>
-            <a href="/exercicio2">Exercicio 2</a>
-        </li>
-    </ul>
-    <ul>
-        <li>
-            <a href="/exercicio3">Exercicio 3</a>
-        </li>
-    </ul>
+    <header>
+        <h1>Bem-vindo à Página de Índice</h1>
+    </header>
+    <main>
+        <section>
+            <h2>Exercicio 1</h2>
+            <ul>
+                <li>
+                    <a href="/Exercicio2/">Exercicio 1</a>
+                </li>
+            </ul>
+        </section>
+        <section>
+            <h2>Exercicio 2</h2>
+            <ul>
+                <li>
+                    <a href="/Exercicio2/">Exercicio 2</a>
+                </li>
+            </ul>
+        </section>
+        <section>
+            <h2>Exercicio 3</h2>
+            <ul>
+                <li>
+                    <a href="/Exercicio3/">Exercicio 3</a>
+                </li>
+            </ul>
+        </section>
+        <section>
+            <h2>Exercicio 4</h2>
+            <ul>
+                <li>
+                    <a href="/Exercicio4/">Exercicio 4</a>
+                </li>
+            </ul>
+        </section>
+        <section>
+            <h2>Exercicio 5</h2>
+            <ul>
+                <li>
+                    <a href="/Exercicio5/">Exercicio 5</a>
+                </li>
+            </ul>
+        </section>
+        <section>
+            <h2>Exercicio 6</h2>
+            <ul>
+                <li>
+                    <a href="/Exercicio6/">Exercicio 6</a>
+                </li>
+            </ul>
+        </section>
+    </main>
+    </main>
+    </main>
 </body>
 </html>
