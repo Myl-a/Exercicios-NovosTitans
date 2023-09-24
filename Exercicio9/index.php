4e2dd2615707a0acd07b4c063a3078cef445804b
Sâmyla Ferreira <samylamaria14@gmail.com>
2023-09-24 19:42:42 -0300

    Colocando o JS em um arquivo separado

--- Exercicio9/index.php
+++ Exercicio9/index.php
@@ -23,25 +23,7 @@
             </div>
         </form>
     </main>
-    <script>
-        const mode = document.getElementById('mode_icon');
-        const container = document.getElementById('container');
-        mode.addEventListener('click', () => {
-            const form = document.getElementById('night_form');
-            if (mode.classList.contains('fa-moon')) {
-                mode.classList.remove('fa-moon');
-                mode.classList.add('fa-sun');
-                form.classList.add('dark');
-                container.style.background = 'var(--color-dark-100)';
-                return;
-            }
-            mode.classList.remove('fa-sun');
-            mode.classList.add('fa-moon');
-
-            form.classList.remove('dark');
-            container.style.background = 'var(--color-gradient)';
-        });
-    </script>
+    <script src="script.js"></script>
 </body>
 </html>
 
