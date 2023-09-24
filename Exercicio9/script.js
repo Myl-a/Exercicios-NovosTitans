05703a4af87025a0f24054a609b5ec6436eb993f
Sâmyla Ferreira <samylamaria14@gmail.com>
2023-09-24 19:47:31 -0300

    Colocando o JS em um arquivo separado

--- Exercicio9/script.js
+++ Exercicio9/script.js
@@ -1,17 +1,16 @@
 const mode = document.getElementById('mode_icon');
     const container = document.getElementById('container');
-    mode.addEventListener('click', () => {
-        const form = document.getElementById('night_form');
-        if (mode.classList.contains('fa-moon')) {
-            mode.classList.remove('fa-moon');
-            mode.classList.add('fa-sun');
-            form.classList.add('dark');
-            container.style.background = 'var(--color-dark-100)';
-            return;
-        }
-        mode.classList.remove('fa-sun');
-        mode.classList.add('fa-moon');
-
-        form.classList.remove('dark');
-        container.style.background = 'var(--color-gradient)';
+        mode.addEventListener('click', () => {
+            const form = document.getElementById('night_form');
+            if (mode.classList.contains('fa-moon')) {
+                mode.classList.remove('fa-moon');
+                mode.classList.add('fa-sun');
+                form.classList.add('dark');
+                container.style.background = 'var(--color-dark-100)';
+                return;
+            }
+            mode.classList.remove('fa-sun');
+            mode.classList.add('fa-moon');
+            form.classList.remove('dark');
+            container.style.background = 'var(--color-gradient)';
 });
