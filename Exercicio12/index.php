49c07c5cf3322bc72ace8d6e5426aa0ad1ebe2e8
Sâmyla Ferreira <samylamaria14@gmail.com>
2023-09-25 11:19:53 -0300

    Adicionando o frontend do exercicio 12

--- Exercicio12/index.php
+++ Exercicio12/index.php
@@ -1,25 +1,21 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    <title>Exercício 12</title>
+	<title> Exercicio 13 </title>
 </head>
-<body>
-    <div class="container">
-        <h1>Exercício 12</h1>
-
-        <form action="index.php" method="POST">
-            <div class="input-field">
-                <input type="text" name="palavra" id="palavra" placeholder="Digite uma palavra" required>
-                <label for="palavra">Palavra</label>
+<body class="blue lighten-4">
+	<form action="process.php" method="post">
+        <div class="section container">
+            <div class="row card-panel">
+                <div class="input-field col s12">
+                    <h4> Contador de Números entre 100 e 200</h4>
+                    <input type="text" name="number" id="number" placeholder="Digite aqui" class="validate">
+                    <button class="btn waves-effect pink waves-light" type="submit" name="action">Contar</button>
+                    <button id="CarregarListagem" class="btn waves-effect pink waves-light" type="submit" name="action">Carregar Listagem</button>
+                </div>
             </div>
-
-            <button class="btn waves-effect waves-light" type="submit">Imprimir</button>
-        </form>
-    </div>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+        </div>
+    </form>
 </body>
 </html>
