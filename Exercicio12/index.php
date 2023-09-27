ac201941cc1ddaa4c0f4f086d5ec2444b85bae37
Sâmyla Ferreira <samylamaria14@gmail.com>
2023-09-27 13:46:02 -0300

    Adicionando o backend do exercicio 12

--- Exercicio12/index.php
+++ Exercicio12/index.php
@@ -1,21 +1,67 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-	<title> Exercicio 13 </title>
+    <title>Exercicio 12</title>
+    <link rel="stylesheet" href="style.css">
+    <style>
+        .caixa {
+            border: 1px solid #000;
+            padding: 10px;
+            margin: 5px;
+        }
+    </style>
 </head>
-<body class="blue lighten-4">
-	<form action="process.php" method="post">
-        <div class="section container">
-            <div class="row card-panel">
-                <div class="input-field col s12">
-                    <h4> Contador de Números entre 100 e 200</h4>
-                    <input type="text" name="number" id="number" placeholder="Digite aqui" class="validate">
-                    <button class="btn waves-effect pink waves-light" type="submit" name="action">Contar</button>
-                    <button id="CarregarListagem" class="btn waves-effect pink waves-light" type="submit" name="action">Carregar Listagem</button>
-                </div>
+<body>
+    <div class="container">
+        <h1>Repetir Palavra</h1>
+        <form method="POST" action="">
+            <div class="form-group">
+                <label for="palavra">Digite uma palavra:</label>
+                <input type="text" name="palavra" id="palavra" placeholder="Digite a palavra" required>
             </div>
+            <div class="form-group">
+                <label for="repeticoes">Número de repetições:</label>
+                <input type="number" name="repeticoes" id="repeticoes" placeholder="Número de repetições" min="1" required>
+            </div>
+            <button type="submit" name="submit">Repetir</button>
+        </form>
+        <div id="loading" style="display: none;">
+            Carregando...
         </div>
-    </form>
+        <div id="output">
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $palavra = $_POST["palavra"];
+            $repeticoes = intval($_POST["repeticoes"]);
+
+            for ($i = 1; $i <= $repeticoes; $i++) {
+                echo '<div class="caixa">';
+                for ($j = 1; $j <= $i; $j++) {
+                    echo '<span class="result chip">' . htmlspecialchars($palavra) . '</span>';
+                }
+                echo '</div>';
+            }
+
+            // Inclua a conexão com o banco de dados e insira os dados com segurança
+            include("conexao.php");
+
+            $palavraSegura = mysqli_real_escape_string($conexao, $palavra); // Evita injeção SQL
+            $sql = "INSERT INTO Exercicio_12 (Palavras, Repetições) VALUES ('$palavraSegura', $repeticoes)";
+
+            if (mysqli_query($conexao, $sql)) {
+                // Inserção bem-sucedida
+            } else {
+                echo "Erro: " . mysqli_error($conexao);
+            }
+
+            mysqli_close($conexao);
+        } else {
+            echo '<span class="result chip">' . "Por favor, preencha o formulário." . '</span> ';
+        }
+        ?>
+        </div>
+        <a href="listagem.php">Ver Listagem</a>
+    </div>
 </body>
 </html>
+
