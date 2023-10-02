5c94d75d31d7a3d766e9f6e6768a890ce3d5b60f
Sâmyla Ferreira <samylamaria14@gmail.com>
2023-10-02 13:53:08 -0300

    Adicionando o frontend do exercicio13

---
+++ Exercicio13/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Exercicio 13</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id="container">
+        <h1>Contador de Números entre 100 e 200</h1>
+        <p>Digite números (0 para encerrar):</p>
+        <form method="post">
+            <input type="number" name="numero">
+            <button type="submit" name="contar">Contar</button>
+        </form>
+        <p id="output">Números entre 100 e 200:</p>
+    </div>
+</body>
+</html>
