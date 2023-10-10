908bcf5c8bb6d1ae78c62a575eacb354c90b7ebd
Sâmyla Ferreira <samylamaria14@gmail.com>
2023-10-10 14:04:41 -0300

    Adicionando o frontend do Exercicio 14

---
+++ Exercicio14/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<head>
+    <link rel="stylesheet" href="style.css">
+    <title>Exercicio 14</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Crescimento de Chico e Juca</h1>
+        <p>Chico tem 1,50m e cresce 2 centímetros por ano.</p>
+        <p>Juca tem 1,10m e cresce 3 centímetros por ano.</p>
+        
+        <form method="post">
+            <button name="calcular">Calcular Anos</button>
+        </form>
+    </div>
+</body>
+</html>
