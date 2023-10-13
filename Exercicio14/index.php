b9072971bb78011d9904c5bd8fc15215d1c189ff
Sâmyla Ferreira <samylamaria14@gmail.com>
2023-10-13 18:23:29 -0300

    Adicionando o backend do exercicio 14

--- Exercicio14/index.php
+++ Exercicio14/index.php
@@ -1,77 +1,54 @@
 <!DOCTYPE html>
-    <head>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-        <title>Exercicio 14</title>
-    </head>
-    <body class="container">
-        <form action="index.php" method="post" class="col s12">
-            <div class="row">
-                <div class="input-field col s6">
-                    <label for="altura_chico">Altura do Chico em cm:</label>
-                    <input type="text" name="altura_chico" id="altura_chico" class="validate" value="150" required>
-                </div>
-                <div class="input-field col s6">
-                    <label for="crescimento_chico">Crescimento anual do Chico:</label>
-                    <input type="text" name="crescimento_chico" id="crescimento_chico" class="validate" value="2" required>
-                </div>
-            </div>
-            <div class="row">
-                <div class="input-field col s6">
-                    <label for="altura_juca">Altura do Juca em cm:</label>
-                    <input type="text" name="altura_juca" id="altura_juca" class="validate" value="110" required>
-                </div>
-                <div class="input-field col s6">
-                    <label for="crescimento_juca">Crescimento anual do Juca:</label>
-                    <input type="text" name="crescimento_juca" id="crescimento_juca" class="validate" value="3" required>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col s12">
-                    <label>Quantos anos serão necessários para que Juca seja maior que Chico?</label>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col s12">
-                    <button class="btn waves-effect pink waves-light" type="submit" name="action">Calcular</button>
-                </div>
-            </div>
+<html>
+<head>
+    <title>Exercicio 14</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Crescimento de Chico e Juca</h1>
+        <form action="index.php" method="post">
+            <label>Altura do Chico em cm:</label>
+            <input type="text" name="alturaChico" id="alturaChico" value="150" required>
+            <label>Crescimento anual do Chico:</label>
+            <input type="text" name="crescimentoChico" id="crescimentoChico" value="2" required>
+            <label>Altura do Juca em cm:</label>
+            <input type="text" name="alturaJuca" id="alturaJuca" value="110" required>
+            <label>Crescimento anual do Juca:</label>
+            <input type="text" name="crescimentoJuca" id="crescimentoJuca" value="3" required>
+            <label>Quantos anos serão necessários para que Juca seja maior que Chico?</label>
+            <br>
+            <input type="submit" value="Calcular">
         </form>
-        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-        <script>
-        $(document).ready(function() {
-            $("#calculo").submit(function(event) {
-                event.preventDefault();
-                var formData = $(this).serialize();
-                $.ajax({
-                    type: "POST", 
-                    url: "process.php", 
-                    data: formData, 
-                    success: function(response) {
-                        console.log("Requisição AJAX enviada com sucesso!");
-                        console.log(response); 
-                        $("#tempo").text("Tempo necessário: " + response + " segundos");
-                    },
-                    error: function(error) {
-                        console.error("Erro na requisição AJAX:");
-                        console.error(error);
+        <?php
+            if (isset($_POST["alturaChico"]) && isset($_POST["alturaJuca"]) && isset($_POST["crescimentoChico"]) && isset($_POST["crescimentoJuca"])) {
+                $alturaChico = $_POST["alturaChico"];
+                $alturaJuca = $_POST["alturaJuca"];
+                $crescimentoChico = $_POST["crescimentoChico"];
+                $crescimentoJuca = $_POST["crescimentoJuca"];
+                function calculo($alturaChico, $alturaJuca, $crescimentoChico, $crescimentoJuca) {
+                    $anos = 0;
+                    while ($alturaJuca <= $alturaChico) {
+                        $alturaChico += $crescimentoChico;
+                        $alturaJuca += $crescimentoJuca;
+                        $anos++;
                     }
-                });
-            });
-            
-        });
-        function carregarListagemDados() {
-                $.ajax({
-                    type: "POST", 
-                    url: "listagem.php",  
-                    success: function(response) {
-                        $("#listagem").html(response);
-                    },
-                    error: function(error) {
-                        console.error("Erro ao carregar a listagem:");
-                        console.error(error);
-                    }
-                });
+                    return $anos;
+                }
+                $anos = calculo($alturaChico, $alturaJuca, $crescimentoChico, $crescimentoJuca);
+                echo '<div class="container">';
+                echo '<p class="flow-text"> Serão necessários ' . $anos . ' anos para que Juca seja maior do que Chico.</p>';
+                echo '<a href="listagem.php" class="btn waves-effect waves-light">Listagem</a>';
+                include("conexao.php");
+                echo '</br>';
+                $sql = "INSERT INTO Exercicio14 (alturaChico, alturaJuca, crescimentoChico, crescimentoJuca, anos) 
+                    VALUES ('$alturaChico', '$alturaJuca', '$crescimentoChico', '$crescimentoJuca', '$anos')";
+                if (mysqli_query($conexao, $sql)) {
+                    echo "";
+                } else {
+                    echo "Erro: " . mysqli_connect_errno($conexao);
+                }
+                mysqli_close($conexao);
             }
             $("#CarregarListagem").click(function() {
                 carregarListagemDados();
