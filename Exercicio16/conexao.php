1afe897304c4db2bcd08394ab0e88e435a4f15e2
Sâmyla Ferreira <samylamaria14@gmail.com>
2023-10-22 21:41:05 -0300

    Adicionando o frontend do exercicio 16

---
+++ Exercicio16/conexao.php
@@ -0,0 +1,14 @@
+<?php
+    $dbHost = 'database';
+    $dbUser = 'sam_user';
+    $dbPassword = 'senha2';
+    $dbName = 'novos_titans';
+
+    $conexao = mysqli_connect($dbHost, $dbUser, $dbPassword, $dbName);
+
+    if ($conexao->connect_errno) {
+        echo "Erro:".$conexao->connect_error;
+    } else {
+        echo "Conectado com o banco de dados";
+    }
+?>
---
+++ Exercicio16/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Verificador de Par ou Ímpar</title>
+    <link rel="stylesheet" href="styles.css">
+<body>
+    <div class="container">
+        <h1>Verificador de Par ou Ímpar</h1>
+        <p>Insira um número e clique no botão para verificar se é par ou ímpar:</p>
+
+        <div>
+            <label for="numero">Número:</label>
+            <input type="number" id="numero" placeholder="Digite um número">
+            <button onclick="verificarParOuImpar()">Verificar</button>
+        </div>
+
+        <div id="resultado"></div>
+    </div>
+</body>
+</html>
