6888bca9b060c45145563d03289d1257512f33af
Sâmyla Ferreira <samylamaria14@gmail.com>
2023-10-25 14:05:06 -0300

    Adicionando o backend do exercicio 16, com a listagem do banco de dados e o teste automatizado

--- Exercicio16/index.php
+++ Exercicio16/index.php
@@ -3,18 +3,59 @@
 <head>
     <title>Exercicio 16</title>
     <link rel="stylesheet" href="styles.css">
+</head>
 <body>
     <div class="container">
         <h1>Verificador de Par ou Ímpar</h1>
-        <p>Insira um número e clique no botão para verificar se é par ou ímpar:</p>
-
+        <p>Insira 15 números separados por vírgula e clique no botão para verificar se são pares ou ímpares:</p>
         <div>
-            <label for="numero">Número:</label>
-            <input type="number" id="numero" placeholder="Digite um número">
-            <button onclick="verificarParOuImpar()">Verificar</button>
+            <form method="post">
+                <label for="numero">Números:</label>
+                <input type="text" name="numero" id="numero" placeholder="Digite os números separados por vírgula"><br>
+                <button type="submit">Verificar</button>
+            </form>
         </div>
-
-        <div id="resultado"></div>
+        <a href="listagem.php">Ver Listagem</a>
+        <div id="resultado">
+            <?php
+            include("conexao.php");
+            function ParOuImpar($numero) {
+                if ($numero % 2 == 0) {
+                    return 'par';
+                } else {
+                    return 'ímpar';
+                }
+            }
+            if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['numero'])) {
+                $numeroString = $_POST['numero'];
+                $numeros = explode(',', $numeroString);
+                if (count($numeros) == 15) {
+                    $resultado = '';
+                    foreach ($numeros as $numero) {
+                        $resultado .= "O número $numero é " . ParOuImpar($numero) . ".<br>";
+                    }
+                    echo $resultado;
+                    if (isset($conexao)) {
+                        $numeroString = mysqli_real_escape_string($conexao, $numeroString);
+                        $resultado = mysqli_real_escape_string($conexao, $resultado);
+                        
+                        $sql = "INSERT INTO Exercicio16 (numero, resultado) VALUES ('$numeroString', '$resultado')";
+                        
+                        if (mysqli_query($conexao, $sql)) {
+                            echo "Salvo";
+                        } else {
+                            echo "Error: " . $sql . "<br>" . mysqli_error($conexao);
+                        }
+                    }
+                } else {
+                    echo "Você deve fornecer exatamente 15 números separados por vírgula.";
+                }
+            }
+            if (isset($conexao)) {
+                mysqli_close($conexao);
+            }
+            ?>
+        </div>
     </div>
 </body>
 </html>
