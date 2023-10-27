ea240f693c72f3ebf2ec63db61cee2923f6428a0
Sâmyla Ferreira <samylamaria14@gmail.com>
2023-10-27 14:21:24 -0300

    Adicionando o frontend do exercicio 17 junto com a conexão do banco de dados

---
+++ Exercicio17/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Exercicio 17</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <h1>Calculadora de Vetor</h1>
+    
+    <form method="post" action="process.php">
+        <label for="numbers">Digite 20 números inteiros separados por vírgula:</label>
+        <input type="text" name="numbers" id="numbers" required><br>
+        <button type="submit">Calcular</button><br>
+        <a href="listagem.php">Ver Listagem</a>
+    </form>
+</body>
+</html>
