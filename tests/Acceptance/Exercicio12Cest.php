05f2374910077e376049e8e36c42e75c802c5b68
Sâmyla Ferreira <samylamaria14@gmail.com>
2023-11-03 20:34:42 -0300

    ALterando o backend do exercicio 12 para que ele se adapte ao novo frontend que usa materialize.css

--- tests/Acceptance/Exercicio12Cest.php
+++ tests/Acceptance/Exercicio12Cest.php
@@ -9,23 +9,18 @@ class Exercicio12Cest
     public function _before(AcceptanceTester $I)
     {
     }
-
-    // Teste automatizado
     public function Exercicio12test(AcceptanceTester $I)
     {
         $I->amOnPage("/Exercicio12");
-        $I->see("Repetir Palavra");
-
         $palavra = "Test";
-        $repeticoes = 4;
-
         $I->fillField("palavra", $palavra);
-        $I->fillField("repeticoes", $repeticoes);
-        $I->click('Repetir');
-
-        $I->see('Test');
-        $I->see('TestTest'); 
-        $I->see('TestTestTest'); 
-        $I->see('TestTestTestTest');
+        $I->click('Imprimir');
+        for ($x = 1; $x <= 4; $x++) {
+            for ($y = 1; $y <= $x; $y++) {
+                $I->see($palavra);
+            }
+            $I->seeInSource('<br>');
+        }
+        $I->see("Voltar");
     }
 }
