bf1ec3582e6d0ecb417dbb0cd4d7ee22192ca079
Sâmyla Ferreira <samylamaria14@gmail.com>
2023-11-06 15:18:35 -0300

    Corrigindo o erro no exercicio 9, não estava imprimindo a palavra SOL, e alterando o teste automatizado que não estava passando

--- Exercicio9/index.php
+++ Exercicio9/index.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
     <link rel="stylesheet" href="style.css">
     <title>Exercicio 9</title>
 </head>
-<body>  
+<body>
     <main id="container">
-        <form id="night_form">
+        <form id="night_form" method="POST">
             <div id="form_header">
                 <h1>Imprimir a palavra SOL</h1>
                 <i id="mode_icon" class="fa-solid fa-moon"></i>
@@ -16,12 +16,28 @@
                 <div class="input-box">
                     <label for="numVezes">Número de vezes:</label>
                     <div class="input-field">
-                        <input type="number" id="numVezes" placeholder="Digite o número de vezes" min="1" required>
+                        <input type="number" name="numVezes" id="numVezes" placeholder="Digite o número de vezes" min="1" required>
                     </div>
                 </div>
                 <button type="submit" id="imprimir_button">Imprimir</button>
             </div>
         </form>
+        <div id="resultado-container">
+            <h1>Resultado</h1>
+            <div id="resultado">
+                <?php
+                if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                    if (isset($_POST['numVezes']) && is_numeric($_POST['numVezes'])) {
+                        $numeroVezes = intval($_POST['numVezes']);
+                        $sol = str_repeat("SOL ", $numeroVezes);
+                        echo $sol;
+                    } else {
+                        echo "Por favor, insira um número válido.";
+                    }
+                }
+                ?>
+            </div>
+        </div>
     </main>
     <script src="script.js"></script>
 </body>
--- tests/Acceptance/Exercicio9Cest.php
+++ tests/Acceptance/Exercicio9Cest.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace Tests\Acceptance;
 
 use Tests\Support\AcceptanceTester;
@@ -9,23 +8,21 @@ class Exercicio9Cest
 {
     public function _before(AcceptanceTester $I)
     {
+        $I->amOnPage('/Exercicio9/index.php');
     }
 
-    
     public function testImprimirPalavra(AcceptanceTester $I)
     {
-        $I->amOnPage('/Exercicio9/index.php');
         $I->fillField('numVezes', '3');
         $I->click('Imprimir');
-        $I->waitForElement('#resultado', 10);
+        $I->seeElement('#resultado');
         $I->see('SOL SOL SOL', '#resultado');
     }
+
     public function testValorInvalido(AcceptanceTester $I)
     {
-        $I->amOnPage('/Exercicio9/index.php');
-        $I->see('Imprimir a palavra SOL');
         $I->fillField('numVezes', 'abc');
         $I->click('Imprimir');
-        $I->waitForText("Por favor, insira um número válido.", 10);
+        $I->see('Por favor, insira um número válido.');
     }
 }
