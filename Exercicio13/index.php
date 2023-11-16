0978d936b7fb3a0bb7c90cf69e7ac784880e1381
Sâmyla Ferreira <samylamaria14@gmail.com>
2023-11-16 14:42:48 -0300

    Atualizando o frontend do exercicio 13 para que tenha o materialize.css

--- Exercicio13/index.php
+++ Exercicio13/index.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Exercicio 13</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+	<title> Exercicio 13 </title>
 </head>
-<body>
-    <div id="container">
-        <h1>Contador de Números entre 100 e 200</h1>
-        <p>Digite números (0 para encerrar):</p>
-        <form method="post">
-            <input type="number" name="numero">
-            <button type="submit" name="contar">Contar</button>
-        </form>
-        <p id="output">Números entre 100 e 200:</p>
-    </div>
+<body class="blue lighten-4">
+	<form action="" method="post">
+        <div class="section container">
+            <div class="row card-panel">
+                <div class="input-field col s12">
+                    <h4> Contador de Números entre 100 e 200</h4>
+                    <input type="text" name="number" id="number" placeholder="Digite aqui" class="validate">
+                    <button class="btn waves-effect pink waves-light" type="submit" name="action">Enviar</button>
+                </div>
+            </div>
+        </div>
+    </form>
 </body>
 </html>
