30ae2e574912818c10b6d0b4a0b00a93134cdab7
Sâmyla Ferreira <samylamaria14@gmail.com>
2023-11-27 15:49:25 -0300

    Corrigindo o exercicio 10, o banco de dados não estava rodando

--- Exercicio10/index.php
+++ Exercicio10/index.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <link rel="stylesheet" href="style.css">
-    <title>Calculadora de Soma de Números</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Exercicio 10</title>
 </head>
 <body>
-    <div class="container">
-        <h2>Calculadora de Soma de Números</h2>
-        <form method="post" action="calculadora.php">
-            <p>Insira 20 números separados por vírgula e clique em calcular:</p>
-            <input type="text" name="inputNumbers" placeholder="Insira os números separados por vírgula">
-            <button class="btn-primary" type="submit">Calcular</button>
-        </form>
-    </div>
+    <form action="process.php" method="get">
+        <label>Informe 20 números (Separados por vírgula)</label>
+        <input type="text" id="numeros" name="numeros">
+        <input type="submit" value="Enviar">
+    </form>
 </body>
 </html>
