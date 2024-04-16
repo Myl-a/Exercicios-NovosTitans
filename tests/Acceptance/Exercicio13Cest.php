5f5b9e0d7f0baa9c0d79d6ce9095298b61adfad1
Sâmyla Ferreira <samylamaria14@gmail.com>
2024-04-16 20:31:22 -0300

    Corrigindo o teste do exercicio 13

--- tests/Acceptance/Exercicio13Cest.php
+++ tests/Acceptance/Exercicio13Cest.php
@@ -10,20 +10,29 @@ class Exercicio13Cest
     public function _before(AcceptanceTester $I)
     {
     }
-
     // tests
     public function Exercicio13Test(AcceptanceTester $I)
     {
-    
         $I->amOnPage('/Exercicio13');
         $I->fillField('number', '150,165');
         $I->click('Contar');
         $I->see("Foi digitado 2 número(s) entre 100 e 200.");
-
-
         $I->amOnPage('/Exercicio13');
         $I->fillField('number', '');
         $I->click('Contar');
         $I->see("Por favor, insira um número válido.");
+    }
+    public function Exercicio13ListagemTest(AcceptanceTester $I)
+    {
+        $I->amOnPage('/Exercicio13');
+        $I->fillField('number', '150,165');
+        $I->click('Contar');
+        $I->see('Os números entre 100 e 200 são:');
+        $I->see('150');
+        $I->see('165');
+        $I->amOnPage('/Exercicio13');
+        $I->fillField('number', '');
+        $I->click('Contar');
+        $I->see('Por favor, insira um número válido.');
     }
 }
