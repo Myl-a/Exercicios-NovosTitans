8f6c391ed43922d01b72b0b33178bd31448fbc0c
Sâmyla Ferreira <samylamaria14@gmail.com>
2024-04-16 20:37:59 -0300

    Adicionando o teste de listagem no exercicio 14

--- tests/Acceptance/Exercicio14Cest.php
+++ tests/Acceptance/Exercicio14Cest.php
@@ -9,9 +9,7 @@ class Exercicio14Cest
     public function _before(AcceptanceTester $I)
     {
     }
-
-    // Teste automatizado
-    public function Exercicio14test(AcceptanceTester $I)
+    public function Exercicio14Test(AcceptanceTester $I)
     {
         $I->amOnPage("/Exercicio14");
         $I->fillField("alturaChico", "150");
@@ -20,5 +18,10 @@ class Exercicio14Cest
         $I->fillField("crescimentoJuca", "3");
         $I->click('Calcular');
         $I->see("Serão necessários 41 anos para que Juca seja maior do que Chico.");
+        $I->see('Listagem dos anos e alturas:');
+        $I->see('Ano: 1 - Altura Chico: 152cm - Altura Juca: 113cm');
+        $I->see('Ano: 2 - Altura Chico: 154cm - Altura Juca: 116cm');
+        $I->see('Ano: 3 - Altura Chico: 156cm - Altura Juca: 119cm');
+        $I->amOnPage("/Exercicio14");
     }
 }
