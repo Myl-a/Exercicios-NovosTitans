0fd1ea005ee0bc4e86787288e0289c1fae0e1b91
Sâmyla Ferreira <samylamaria14@gmail.com>
2024-06-05 18:28:25 -0300

    Adicionando o teste de listagem do exercicio 16

--- tests/Acceptance/Exercicio16Cest.php
+++ tests/Acceptance/Exercicio16Cest.php
@@ -10,8 +10,8 @@ class Exercicio16Cest
     {
     }
 
-    // tests
-    public function Exercicio16Test(AcceptanceTester $I)
+    // testes
+    public function FromExercicio16Test(AcceptanceTester $I)
     {
         $I->amOnPage('/Exercicio16');
 
@@ -20,6 +20,29 @@ class Exercicio16Cest
         $I->see("O número 3 é ímpar.O número 6 é par.O número 10 é par.O número 5 é ímpar.O número 7 é ímpar.O número 9 é ímpar.O número 8 é par.O número 1 é ímpar.O número 4 é par.O número 25 é ímpar.O número 16 é par.O número 13 é ímpar.O número 17 é ímpar.O número 89 é ímpar.O número 15 é ímpar.");
 
         $I->seeInDatabase('exercicio16', ['numeros' => '3,6,10,5,7,9,8,1,4,25,16,13,17,89,15', 'resposta' => 'O número 3 é ímpar.<br>O número 6 é par.<br>O número 10 é par.<br>O número 5 é ímpar.<br>O número 7 é ímpar.<br>O número 9 é ímpar.<br>O número 8 é par.<br>O número 1 é ímpar.<br>O número 4 é par.<br>O número 25 é ímpar.<br>O número 16 é par.<br>O número 13 é ímpar.<br>O número 17 é ímpar.<br>O número 89 é ímpar.<br>O número 15 é ímpar.<br>']);
-        
+    }
+
+    public function FromListagem(AcceptanceTester $I)
+    {
+        $I->amOnPage('/Exercicio16');
+
+        $I->click('Carregar Listagem');
+        $I->amOnPage('/Exercicio16');
+        $I->see('Números: 3,6,10,5,7,9,8,1,4,25,16,13,17,89,15');
+        $I->see('O número 3 é ímpar.');
+        $I->see('O número 6 é par.');
+        $I->see('O número 10 é par.');
+        $I->see('O número 5 é ímpar.');
+        $I->see('O número 7 é ímpar.');
+        $I->see('O número 9 é ímpar.');
+        $I->see('O número 8 é par.');
+        $I->see('O número 1 é ímpar.');
+        $I->see('O número 4 é par.');
+        $I->see('O número 25 é ímpar.');
+        $I->see('O número 16 é par.');
+        $I->see('O número 13 é ímpar.');
+        $I->see('O número 17 é ímpar.');
+        $I->see('O número 89 é ímpar.');
+        $I->see('O número 15 é ímpar.');
     }
 }
